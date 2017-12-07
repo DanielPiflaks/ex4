77c7bb44314c462994aa33af176f0904cda71c93
danielpiflaks <danielpiflaks@gmail.com>
2017-12-07 22:34:38 +0200

    Adding try - catch for server

--- src/server/main_server.cpp
+++ src/server/main_server.cpp
@@ -2,14 +2,23 @@
 // Created by danielpiflaks on 02/12/17.
 //
 
+#include <iostream>
+#include <cstdlib>
 #include "Server.h"
 
 int main() {
-    const char * fileName = "ServerConnectionSettings.txt";
-    Server serverGame(fileName);
-    serverGame.start();
-    serverGame.notifyFirstPlayerStart();
-    serverGame.sendAndReceiveMoves();
-    serverGame.stop();
+    const char *fileName = "ServerConnectionSettings.txt";
+    try {
+        Server serverGame(fileName);
+        serverGame.start();
+        serverGame.notifyFirstPlayerStart();
+        serverGame.sendAndReceiveMoves();
+        serverGame.stop();
+
+    } catch (const char *msg) {
+        cout << "Cannot start server. Reason: " << msg << endl;
+        exit(-1);
+    }
+
     return 0;
 }
